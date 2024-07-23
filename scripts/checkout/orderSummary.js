c128d4571b5c33945a16f07d02780e07ad5b2694
Farshad Davari <DavariFarshad3@outlook.com>
2024-07-23 11:37:24 +0330

    Add 5 day in new format to code 15a.

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -140,3 +140,14 @@ export function renderOrderSummary() {
       })
     });
 }
+
+const today = dayjs();
+const deliveryDate = today.add(
+  5,
+  "days"
+);
+const dateString = deliveryDate.format(
+  "MMMM D"
+);
+
+console.log(dateString)
